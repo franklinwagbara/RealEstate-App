37c3c16d8478863c8c5f41e173095189579fb95f
Franklin Wagbara <wagbarafranklin@yahoo.com>
2022-04-14 09:48:52 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import MAT from "./MAT";
-import Mui, { App2 } from "./Mui";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
